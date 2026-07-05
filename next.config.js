4ba2d56cd7ef4fb3b121e1585424e224975347d4
Akshat Arora <107341281+CyberBoy-Akshat@users.noreply.github.com>
2026-07-05 20:51:14 +0530

    Remove Next.js configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "*" },
-          { key: "Content-Security-Policy", value: "upgrade-insecure-requests" }
-        ],
-      },
-    ];
-  },
-};
 
-export default nextConfig;
